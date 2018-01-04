94c78ae242c9ed247b230c1c16af42d23b26c1a7
duduluu <duduluu@outlook.com>
2018-01-04 18:43:20 +0800

    Add Control VdAcrylic, VdSection, VdContainer

---
+++ src/controls/style/acrylic/VdAcrylic.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="vd-acrylic">
+    <div class="vd-acrylic_background"
+         :style="backgroundStyle"></div>
+    <div class="vd-acrylic_cover"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue,
+  Emit,
+  Inject,
+  Model,
+  Prop,
+  Provide,
+  Watch,
+} from 'vue-property-decorator';
+import { VdStylableControl } from 'src/controls/base/VdControl';
+
+@Component
+export default class VdAcrylic extends VdStylableControl {
+  @Prop() backgroundImage: string;
+
+  get backgroundStyle() {
+    return {
+      backgroundImage: `url("${this.backgroundImage}")`,
+    };
+  }
+
+  // get classes(): ClassNames {
+  //   return [`theme-${this.theme || this.$void.theme}`];
+  // }
+}
+</script>
