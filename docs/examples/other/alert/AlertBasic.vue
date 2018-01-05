088cda4772b33814f759ddf3ffe1ed3233ecc074
winda0612 <18510525641@163.com>
2018-01-05 10:18:31 +0800

    changed 2 files

--- docs/examples/other/alert/AlertBasic.vue
+++ docs/examples/other/alert/AlertBasic.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <vd-button @click="onClick"
-               :disabled="bOk">点击</vd-button>
+    <vd-button @click="onClick">点击</vd-button>
   </div>
 </template>
 <script lang="ts">
@@ -11,21 +10,14 @@ import { Vue, Component } from 'vue-property-decorator';
 
 @Component
 export default class AlertBasic extends Vue {
-  bOk = false;
   onClick() {
     let alert = new VdAlert();
-    this.bOk = !this.bOk;
-    if (this.bOk) {
-      alert.show({
-        preset: 'success',
-        label: '成功提示的标题',
-        description: '成功提示的文案',
-        autoClose: true,
-      });
-      setTimeout(() => {
-        this.bOk = false;
-      }, 3000);
-    }
+    alert.show({
+      preset: 'success',
+      label: '成功提示的标题',
+      description: '成功提示的文案',
+      autoClose: true,
+    });
   }
 }
 </script>
