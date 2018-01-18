25c5fd713fef8435346b49e139a48fea69d1630d
duduluu <duduluu@outlook.com>
2018-01-18 18:05:28 +0800

    Update common layout controls

--- src/controls/layout/common/VdSection.vue
+++ src/controls/layout/common/VdSection.vue
@@ -6,13 +6,21 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
 import { VdStylableControl } from 'src/controls/base/VdControl';
 
 @Component
 export default class VdSection extends VdStylableControl {
+  @Prop({ default: true })
+  transparent: boolean;
+
   get classes(): ClassNames {
-    return [`theme-${this.theme || this.$void.theme}`];
+    return [
+      `theme-${this.theme || this.$void.theme}`,
+      {
+        transparent: this.transparent,
+      },
+    ];
   }
 }
 </script>
