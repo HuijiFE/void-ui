5ab5a79f9c4a43a3b6f30ba68356096d3f48d2cb
jsm1003 <838432498@qq.com>
2018-01-19 15:04:47 +0800

    add dataTable style

--- src/controls/data/dataTable/vdDataTable.vue
+++ src/controls/data/dataTable/vdDataTable.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="vd-data-table">
+  <div class="vd-data-table" :class="classes">
   <!-- 表格头部 -->
     <div class="table-head">
     <slot v-for="(hItem, index) in cloneHeadData" name="item-head" :headItem="hItem">
-      <div class="cell head-item">
-        <span @click="getOriginSortData">{{hItem.text}}</span>
+      <div class="head-item">
+        <span class="item-text" @click="getOriginSortData">{{hItem.text}}</span>
         <div class="arrow-control">
-          <span @click="sort('asc', hItem.value)"><i class="fa fa-caret-up" aria-hidden="true"></i></span>
-          <span @click="sort('desc',hItem.value)"><i class="fa fa-caret-down" aria-hidden="true"></i></span>
+          <i @click="sort('asc', hItem.value)" class="fa fa-sort-asc" aria-hidden="true"></i>
+          <i @click="sort('desc',hItem.value)" class="fa fa-sort-desc" aria-hidden="true"></i>
         </div>
       </div>
     </slot>
     </div>
 
     <!-- 表格主体 -->
-<div class="table-body">
+<div class="table-body" :class="{striped: striped}">
     <div class="body-item" v-for="(column, index) in cloneBodyData" :key="index">
   <slot v-for="(hItem, index) in headData" name="item-body" :bodyItem="column" >
-    <div class="cell body-item">{{column[hItem.value]}}</div>
+    <div class="cell item-content">{{column[hItem.value]}}</div>
   </slot>
 </div>
   </div>
@@ -37,13 +37,6 @@ import {
 } from 'vue-property-decorator';
 import { VdStylableControl } from 'src/controls/base/VdControl';
 
-let copy = (obj: any) => {
-  return obj.map(<T extends any>(el: T, index: number) => {
-    el.vd_index = index;
-    return el;
-  });
-};
-
 let sortMap = new Map();
 sortMap.set('asc', (a: number, b: number) => a < b);
 sortMap.set('desc', (a: number, b: number) => b < a);
@@ -56,13 +49,28 @@ export default class VdDataTable extends VdStylableControl {
   @Prop({ default: () => [], type: Array })
   bodyData: object[];
 
-  // 这么 clone 行吗？ 应该用get属性比较好
-  cloneBodyData = copy(this.bodyData);
-  cloneHeadData = copy(this.headData);
+  @Prop({ default: true, type: Boolean })
+  striped: boolean;
 
+  get cloneBodyData() {
+    return this.copy(this.bodyData);
+  }
+  get clondHeadData() {
+    return this.copy(this.headData);
+  }
+  get classes(): ClassNames {
+    return [`theme-${this.theme || this.$void.theme}`];
+  }
+
+  copy(data: object[]) {
+    return data.map(<T extends any>(el: T, index: number) => {
+      el.vd_index = index;
+      return el;
+    });
+  }
   // 排序
   sort(rule: string, value: string) {
-    this.cloneBodyData.sort(<T extends any>(a: T, b: T) => {
+    this.cloneBodyData.sort((a: any, b: any) => {
       return a[value] === b[value] ? 0 : sortMap.get(rule)(a[value], b[value]) ? -1 : 1;
     });
   }
