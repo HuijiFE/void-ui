710d5cbe682951947e4679bf80c92e52d8ffd9b7
duduluu <duduluu@outlook.com>
2018-09-02 21:34:31 +0800

    update frame

--- vue.config.js
+++ vue.config.js
@@ -55,9 +55,7 @@ module.exports = {
       .set('@docs', resolve('docs'))
       .set('@void/ui/lib', resolve('lib'));
 
-    if (VUE_ENTRY === 'docs' && NODE_ENV === 'production') {
-      chainWebpackBuildDocs(config);
-    }
+    docsChainWebpack(config);
   },
 
   parallel: require('os').cpus().length > 1,
@@ -84,18 +82,22 @@ module.exports = {
 
 // ======== Docs ========
 
+// base64 inline encoding file size limit
+const limit = 4096;
+
+// hash in filename
+const hashFunction = 'sha512';
+const hashDigest = 'hex';
+const hashDigestLength = 128;
+
 /**
  * Resolve webpack config by webpack-chain for docs
  * @param {Config} config
  */
-function chainWebpackBuildDocs(config) {
-  // base64 inline encoding file size limit
-  const limit = 4096;
-
-  // hash in filename
-  const hashFunction = 'sha512';
-  const hashDigest = 'hex';
-  const hashDigestLength = 128;
+function docsChainWebpack(config) {
+  if (VUE_ENTRY !== 'docs') {
+    return;
+  }
 
   const isLegacyBundle =
     process.env.VUE_CLI_MODERN_MODE && !process.env.VUE_CLI_MODERN_BUILD;
@@ -103,12 +105,63 @@ function chainWebpackBuildDocs(config) {
   const context = config.store.get('context');
   const resolve = (...paths) => path.resolve(context, ...paths);
 
+  // Customize assets output file name with hash.
+  [
+    ['images', 'url-loader', 'url-loader', limit, 'img'],
+    ['svg', 'file-loader', 'file-loader', undefined, 'img'],
+    ['media', 'url-loader', 'url-loader', limit, 'media'],
+    ['fonts', 'url-loader', 'url-loader', limit, 'fonts'],
+  ].forEach(([rule, use, loader, limit, dir]) =>
+    config.module
+      .rule(rule)
+      .use(use)
+      .loader(loader)
+      .tap(opt => {
+        opt.limit = limit;
+        opt.name = `${
+          configDocs.assetsDir
+        }/${dir}/[name].[${hashFunction}:hash:${hashDigest}:${hashDigestLength}].[ext]`;
+        if (opt.fallback && opt.fallback.options && opt.fallback.options.name) {
+          opt.fallback.options.name = opt.name;
+        }
+
+        return opt;
+      }),
+  );
+
+  if (NODE_ENV !== 'production') {
+    return;
+  }
+
   // Revise entry
   config.entryPoints
     .delete('app')
     .end()
     .entry('docs')
     .add(resolve('docs/main.ts'));
+
+  // Customize js output file name with hash.
+  const jsFilename = isLegacyBundle
+    ? `${configDocs.assetsDir}/js/[name]-legacy.[chunkhash].js`
+    : `${configDocs.assetsDir}/js/[name].[chunkhash].js`;
+  config.output
+    .filename(jsFilename)
+    .chunkFilename(jsFilename)
+    .hashFunction(hashFunction)
+    .hashDigest(hashDigest)
+    .hashDigestLength(hashDigestLength);
+
+  // Customize css output file name with hash.
+  const cssFilename = `${configDocs.assetsDir}/css/[name].[contenthash].css`;
+  config.plugin('extract-css').tap(args => [
+    {
+      filename: cssFilename,
+      chunkFilename: cssFilename,
+      hashFunction,
+      hashDigest,
+      hashDigestLength,
+    },
+  ]);
 
   // Split chunks
   // Chunk splitting
@@ -135,53 +188,6 @@ function chainWebpackBuildDocs(config) {
       },
     },
   });
-
-  // Customize js output file name with hash.
-  const jsFilename = isLegacyBundle
-    ? `${configDocs.assetsDir}/js/[name]-legacy.[chunkhash].js`
-    : `${configDocs.assetsDir}/js/[name].[chunkhash].js`;
-  config.output
-    .filename(jsFilename)
-    .chunkFilename(jsFilename)
-    .hashFunction(hashFunction)
-    .hashDigest(hashDigest)
-    .hashDigestLength(hashDigestLength);
-
-  // Customize css output file name with hash.
-  const cssFilename = `${configDocs.assetsDir}/css/[name].[contenthash].css`;
-  config.plugin('extract-css').tap(args => [
-    {
-      filename: cssFilename,
-      chunkFilename: cssFilename,
-      hashFunction,
-      hashDigest,
-      hashDigestLength,
-    },
-  ]);
-
-  // Customize assets output file name with hash.
-  [
-    ['images', 'url-loader', 'url-loader', limit, 'img'],
-    ['svg', 'file-loader', 'file-loader', undefined, 'img'],
-    ['media', 'url-loader', 'url-loader', limit, 'media'],
-    ['fonts', 'url-loader', 'url-loader', limit, 'fonts'],
-  ].forEach(([rule, use, loader, limit, dir]) =>
-    config.module
-      .rule(rule)
-      .use(use)
-      .loader(loader)
-      .tap(opt => {
-        opt.limit = limit;
-        opt.name = `${
-          configDocs.assetsDir
-        }/${dir}/[name].[${hashFunction}:hash:${hashDigest}:${hashDigestLength}].[ext]`;
-        if (opt.fallback && opt.fallback.options && opt.fallback.options.name) {
-          opt.fallback.options.name = opt.name;
-        }
-
-        return opt;
-      }),
-  );
 
   // Copy examples source code
   if (!isLegacyBundle) {
