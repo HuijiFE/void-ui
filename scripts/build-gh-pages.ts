2eefc9fb442b2e905ad5d842411aeccf6383e222
duduluu <duduluu@outlook.com>
2018-09-11 17:08:05 +0800

    update ci

---
+++ scripts/build-gh-pages.ts
@@ -0,0 +1,76 @@
+/**
+ * Auto copy index.html for gh-pages
+ */
+
+import * as fs from 'fs';
+import * as p from 'path';
+import * as globby from 'globby';
+import * as mkdirp from 'mkdirp';
+
+function resolvePath(...paths: string[]): string {
+  return p.resolve(__dirname, '../', ...paths);
+}
+
+async function copy(): Promise<void> {
+  const articles: string[][] = await Promise.all(
+    ['zh-CN'].map(lang =>
+      globby(resolvePath(`docs/articles/${lang}/**/*.md`)).then(paths =>
+        paths.map(path =>
+          path
+            .replace(resolvePath(`docs/articles/${lang}/`), '')
+            .replace(/^\//, '')
+            .replace(/\.md$/, ''),
+        ),
+      ),
+    ),
+  );
+
+  const indexPath: string = resolvePath('www/client/index.html');
+  if (!fs.existsSync(indexPath)) {
+    throw new Error(`No such file: ${indexPath}`);
+  }
+
+  const paths: string[] = ['zh-CN']
+    .map((lang, index) => [
+      ...articles[index].map(a =>
+        resolvePath('www/client', 'components', lang, a, 'index.html'),
+      ),
+    ])
+    .reduce<string[]>((result, cur) => {
+      result.push(...cur);
+
+      return result;
+    }, []);
+
+  await Promise.all(
+    paths.map(
+      path =>
+        new Promise((resolve, reject) => {
+          const dir: string = p.dirname(path);
+          fs.exists(dir, exists => {
+            if (exists) {
+              fs.copyFile(
+                indexPath,
+                path,
+                cpError => (cpError ? reject(cpError) : resolve()),
+              );
+            } else {
+              mkdirp(
+                dir,
+                mkError =>
+                  mkError
+                    ? reject(mkError)
+                    : fs.copyFile(
+                        indexPath,
+                        path,
+                        cpError => (cpError ? reject(cpError) : resolve()),
+                      ),
+              );
+            }
+          });
+        }),
+    ),
+  );
+}
+
+copy().catch(console.error);
