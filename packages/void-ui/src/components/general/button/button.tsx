c4fd4b536dc4df85c319e95dbb9ddb3d2f14c7c5
duduluu <duduluu@outlook.com>
2018-10-15 05:47:22 +0800

    WIP: button-group

--- packages/void-ui/src/components/general/button/button.tsx
+++ packages/void-ui/src/components/general/button/button.tsx
@@ -77,7 +77,7 @@ export class VdButtonGroup extends Vue implements ThemeComponent {
 
   private beforeCreate(): void {
     let parent: Vue = this.$parent;
-    for (let index = 0; index < 3; index++) {
+    for (let index = 0; index < 5; index++) {
       if (!parent) {
         break;
       }
