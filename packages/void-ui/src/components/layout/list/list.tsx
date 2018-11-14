6012281302c92c0928922b750972317cfc40fef9
duduluu <duduluu@outlook.com>
2018-11-14 20:18:22 +0800

    feat: complete comp/layout/list

--- packages/void-ui/src/components/layout/list/list.tsx
+++ packages/void-ui/src/components/layout/list/list.tsx
@@ -8,14 +8,46 @@ import {
   Provide,
   Watch,
 } from 'vue-property-decorator';
+import { ClassName, Theme, ThemeComponent, LinkLikeComponent } from '../../base';
 
 /**
  * Component: List
  */
 @Component
-export class VdList extends Vue {
+export class VdList extends Vue implements ThemeComponent {
+  @Prop({ type: String })
+  public readonly theme?: Theme;
+  public get themeValue(): Theme {
+    return this.theme || this.$vd_theme.theme;
+  }
+
+  @Prop({ type: [Boolean, Number], default: false })
+  public readonly raise!: boolean | number;
+
+  @Prop({ type: Boolean, default: false })
+  public readonly gap!: boolean;
+
+  @Prop({ type: Boolean, default: false })
+  public readonly divided!: boolean;
+
+  public get classes(): ClassName {
+    return [
+      `vdp-theme_${this.themeValue}`,
+      {
+        'is-raise': this.raise && typeof this.raise === 'boolean',
+        [`vdp-raise_${this.raise}`]: this.raise && typeof this.raise === 'number',
+        'is-gap': this.gap,
+        'is-divided': this.divided,
+      },
+    ];
+  }
+
   private render(h: CreateElement): VNode {
-    return <ul staticClass="vd-list">{this.$slots.default}</ul>;
+    return (
+      <ul staticClass="vd-list" class={this.classes}>
+        {this.$slots.default}
+      </ul>
+    );
   }
 }
 
@@ -23,8 +55,45 @@ export class VdList extends Vue {
  * Component: ListItem
  */
 @Component
-export class VdListItem extends Vue {
+export class VdListItem extends Vue implements LinkLikeComponent {
+  @Prop()
+  // tslint:disable-next-line:no-any
+  public readonly contentClass?: any;
+
+  @Prop({ type: String, default: 'div' })
+  public readonly tag!: string;
+  @Prop({ type: Boolean, default: false })
+  public readonly routerLink!: boolean;
+
   private render(h: CreateElement): VNode {
-    return <li staticClass="vd-list_item">{this.$slots.default}</li>;
+    return (
+      <li staticClass="vd-list_item">
+        {h(
+          this.routerLink ? 'router-link' : this.tag,
+          {
+            staticClass: 'vd-list_item-container',
+            attrs: this.$attrs,
+            props: this.routerLink
+              ? {
+                  tag: this.tag,
+                  ...this.$attrs,
+                }
+              : undefined,
+          },
+          [
+            this.$slots.left && (
+              <div staticClass="vd-list_item-left">{this.$slots.left}</div>
+            ),
+            /* tslint:disable-next-line:no-unsafe-any */
+            <div staticClass="vd-list_item-content" class={this.contentClass}>
+              {this.$slots.default}
+            </div>,
+            this.$slots.right && (
+              <div staticClass="vd-list_item-right">{this.$slots.right}</div>
+            ),
+          ],
+        )}
+      </li>
+    );
   }
 }
