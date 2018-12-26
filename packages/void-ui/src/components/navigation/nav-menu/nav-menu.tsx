1a3921c47aed885d07817fbe2284f93068e70e0c
duduluu <duduluu@outlook.com>
2018-12-26 16:10:49 +0800

    feat: add component nav-menu

--- packages/void-ui/src/components/navigation/nav-menu/nav-menu.tsx
+++ packages/void-ui/src/components/navigation/nav-menu/nav-menu.tsx
@@ -28,14 +28,22 @@ export class VdNavMenu extends Vue implements ThemeComponent {
     return this.theme || (this.$vd_theme && this.$vd_theme.theme) || 'lite';
   }
 
+  @Prop(String)
+  public readonly size?: Size;
+
   public get classes(): ClassName {
-    return [`vdp-theme_${this.themeValue}`];
+    return [
+      `vdp-theme_${this.themeValue}`,
+      {
+        [`vdp-size_${this.size}`]: this.size,
+      },
+    ];
   }
 
   private render(h: CreateElement): VNode {
     return (
       <div staticClass="vd-nav-menu" class={this.classes}>
-        {this.$slots.default}
+        <div staticClass="vd-nav-menu_wrapper">{this.$slots.default}</div>
       </div>
     );
   }
@@ -51,11 +59,29 @@ export class VdNavMenuItem extends Vue implements LinkLikeComponent {
   @Prop({ type: Boolean, default: false })
   public readonly routerLink!: boolean;
 
+  @Prop({ type: Boolean, default: false })
+  public readonly active!: boolean;
+
+  public get classes(): ClassName {
+    return [
+      {
+        'is-active': this.active,
+      },
+    ];
+  }
+
   private render(h: CreateElement): VNode {
     return h(
-      this.tag,
+      this.routerLink ? 'router-link' : this.tag,
       {
         staticClass: 'vd-nav-menu_item',
+        class: this.classes,
+        props: this.routerLink
+          ? {
+              tag: this.tag,
+              ...this.$attrs,
+            }
+          : undefined,
       },
       this.$slots.default,
     );
